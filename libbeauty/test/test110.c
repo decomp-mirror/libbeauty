e96270176adb8a78ef12b965b0ca32e5a5e2b09b
James Courtier-Dutton <James.Dutton@gmail.com>
2018-09-28 16:42:28 +0100

    Added test110.c

--- libbeauty/test/test110.c
+++ libbeauty/test/test110.c
@@ -1,9 +1,10 @@
-#include "global_struct.h"
+//#include "global_struct.h"
 #include <stdio.h>
 
 int test110() {
 	//struct self_s *self;
-	//puts("Hello");
+	puts("Hello");
 	putchar(0x64);
+	printf("Hello2");
 	return 0;
 }
